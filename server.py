08d14b935178b913c15357c1105e0847dcd76477
canofranck <cano.franck77@gmail.com>
2024-05-15 17:43:20 +0200

    fix not use more pts and other case

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ app.secret_key = "something_special"
 
 competitions = load_competitions()
 clubs = load_clubs()
+totalPlacesReserved = {}
 
 
 @app.route("/")
@@ -16,7 +17,7 @@ def index():
 
 @app.route("/showSummary", methods=["POST"])
 def show_summary():
-
+    print(totalPlacesReserved)
     try:
         club = [
             club for club in clubs if club["email"] == request.form["email"]
@@ -36,6 +37,7 @@ def show_summary():
 
 @app.route("/book/<competition>/<club>")
 def book(competition, club):
+    print(totalPlacesReserved)
     try:
 
         foundClub = [c for c in clubs if c["name"] == club][0]
@@ -87,26 +89,62 @@ def book(competition, club):
 
 @app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
+    print(totalPlacesReserved)
     competition = [
         c for c in competitions if c["name"] == request.form["competition"]
     ][0]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
 
-    placesRequired = int(request.form["places"])
-    if placesRequired > 12:
-        flash("You can't book more than 12 places in a competition.", "error")
+    placesRequired = (
+        int(request.form["places"]) if request.form["places"] else None
+    )
+    placesRemaining = int(competition["numberOfPlaces"])
+    if placesRequired is None:
+        flash("Please enter the number of places to reserve.", "error")
         return (
             render_template(
                 "booking.html", club=club, competition=competition
             ),
             400,
         )
+    # Vérifiez si la compétition est déjà dans la structure de données totalPlacesReserved
+    if request.form["competition"] not in totalPlacesReserved:
+        totalPlacesReserved[request.form["competition"]] = 0
 
-    competition["numberOfPlaces"] = (
-        int(competition["numberOfPlaces"]) - placesRequired
-    )
-    club["points"] = int(club["points"]) - placesRequired
-    flash("Great-booking complete!")
+    totalPlacesForCompetition = totalPlacesReserved[
+        request.form["competition"]
+    ]
+
+    if totalPlacesForCompetition == 12:
+        flash(
+            "You have already booked 12 places for this competition.", "error"
+        )
+        return render_template(
+            "welcome.html", club=club, competitions=competitions
+        )
+
+    if placesRequired > int(club["points"]):
+        flash("You don't have enough points.", "error")
+    elif placesRequired > placesRemaining:
+        flash(
+            "Not enough places available, you are trying to book more than the remaining places.",
+            "error",
+        )
+    elif placesRequired < 0:
+        flash("You can't book a negative number of places.", "error")
+    elif placesRequired > 12:
+        flash("You can't book more than 12 places in a competition.", "error")
+    elif totalPlacesForCompetition + placesRequired > 12:
+        flash(
+            "You can't book more than 12 places for this competition.", "error"
+        )
+    else:
+        totalPlacesReserved[request.form["competition"]] += placesRequired
+        competition["numberOfPlaces"] = (
+            int(competition["numberOfPlaces"]) - placesRequired
+        )
+        club["points"] = int(club["points"]) - placesRequired
+        flash("Great-booking complete!")
 
     return render_template(
         "welcome.html", club=club, competitions=competitions
